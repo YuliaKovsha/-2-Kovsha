1118a5fdf7c4db2a7b63d1822c5154c08fb28fdc
YuliaKovsha <juliakovsha@gmail.com>
2025-09-27 17:31:39 +0300

    Create Time.h

---
+++ Time.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Time {
+private:
+    int hours;
+    int minutes;
+    int seconds;
+
+public: //метод ініціалізації
+    void Init(int h, int m, int s);
+    void Read();//введення з клавіатури
+    void Display();//виведення на екран
+    string toString();//перетворення в рядок
+
+    int toSeconds();
+    int toMinutes();
+
+    void addSeconds(int sec);
+    void subtractSeconds(int sec);
+    int differenceInSeconds(Time t);
+
+    bool isEqual(Time t);
+    bool isLess(Time t);
+    bool isGreater(Time t);
+};
